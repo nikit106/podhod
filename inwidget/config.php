8387d98a5ce46ae6602bae8018ecd16d899d9d52
nikit106 <nikit106@gmail.com>
2018-01-15 15:27:57 +0300

    insta2

--- inwidget/config.php
+++ inwidget/config.php
@@ -6,7 +6,7 @@
 $CONFIG = array(
 
 	// Instagram login
-	'LOGIN' => 'podhod.passport',
+	'LOGIN' => 'nickomarov',
 
 	// Get pictures from WORLDWIDE by hashtags. 
 	// Separate hashtags by comma. For example: girl, man
